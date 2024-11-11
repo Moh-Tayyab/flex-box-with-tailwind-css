bb3b65d4d73946ae9b0e6d83717dce38dd2c88c9
Muhammad-Tayyab <m.tayyab1263@gmail.com>
2024-11-11 23:55:04 +0500

    flex-box

---
+++ src/components/Cards.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <main className="flex gap-8 mx-10 my-5">
+          <div className="w-1/3 bg-orange-950 h-96"></div>
+          <div className="w-1/3 bg-purple-950 h-96"></div>
+          <div className="w-1/3 bg-green-950 h-96"></div>  
+      </main>
+  )
+}
+
+export default Cards
---
+++ src/components/SliderImages.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SliderImages = () => {
+  return (
+    <main className='flex gap-5 mx-10 '>
+        <div className='h-60 w-1/2 bg-teal-600'>
+        <h2 className='text-white text-2xl justify-start'>Image1</h2></div>
+        <div className='h-60 w-1/2 bg-orange-700'>
+        <h2 className='text-white text-2xl justify-start'>Image2</h2>
+        </div>
+    </main>
+  )
+}
+
+export default SliderImages
